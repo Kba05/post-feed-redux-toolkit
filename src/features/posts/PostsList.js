caf2f405d63674f1bdd0e1589b2949e1e605655d
Kba05 <babahan-98@mail.ru>
2022-07-06 17:34:34 +0300

    added reaction buttons and time ago component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,16 +2,20 @@ import React from "react"
 import { useSelector } from "react-redux/es/exports"
 import { Link } from 'react-router-dom'
 import { PostAuthor } from "./PostAuthor"
+import { ReactionButtons } from "./ReactionButtons"
+import { TimeAgo } from "./TimeAgo"
 
 export const PostList = () =>{
     const posts = useSelector(state => state.posts)
-    const renderedPosts = posts.map(post=>(
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    const renderedPosts = orderedPosts.map(post=>(
         <article key={post.id} className="post">
 
             <h3>{post.title}</h3>
+            <p><TimeAgo timestamp={post.date}/></p> 
             <PostAuthor userId={post.user}/>
             <p>{post.content}</p>
-
+            <ReactionButtons post={post}/>
             <Link to={`/posts/${post.id}`}>
                 Open post
             </Link>
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useSelector,  } from 'react-redux'
 import { useParams, Link } from 'react-router-dom'
 import { PostAuthor} from './PostAuthor'
+import { ReactionButtons } from "./ReactionButtons"
+import { TimeAgo } from "./TimeAgo"
 
 export const SinglePostPage = () => {
     const { postId } = useParams();
@@ -21,8 +23,10 @@ export const SinglePostPage = () => {
         <section>
             <article className="">
                 <h2>{post.title}</h2>
+                <p><TimeAgo timestamp={post.date}/></p> 
                 <PostAuthor userId={post.user}/>
                 <p className="post-content">{post.content}</p>
+                <ReactionButtons post={post}/>
                 <Link to={`/editPost/${postId}`}>Edit</Link>
                 <p></p>
                 <Link to={`/`}>to list</Link>
